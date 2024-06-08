23de7e09a82a2b8fa7f73b2a2661dc628adfc8d7
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-07 21:42:16 -0400

    routes and server set up

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const app = express();
+
+app.get("/mean");
+
+app.get("/median");
+
+app.get("/mode");
+
+app.listen(3000, function () {
+  console.log(`Server started on port 3000`);
+});

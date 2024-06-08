0145fc18a705fcee4ae578a50cb8e560a1af3c1b
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-07 21:44:49 -0400

    adding function req res next

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/mean");
+app.get("/mean", function (req, res, next) {});
 
-app.get("/median");
+app.get("/median", function (req, res, next) {});
 
-app.get("/mode");
+app.get("/mode", function (req, res, next) {});
 
 app.listen(3000, function () {
   console.log(`Server started on port 3000`);

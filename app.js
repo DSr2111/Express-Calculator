c44c7e53b014bdc528a2b26512ae4c9a5b8347e9
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-08 22:23:28 -0400

    numsAsStrings add

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/mean", function (req, res, next) {});
+app.get("/mean", function (req, res, next) {
+  let numsAsStrings = req.query.nums.split(",");
+});
 
-app.get("/median", function (req, res, next) {});
+app.get("/median", function (req, res, next) {
+  let numsAsStrings = req.query.nums.split(",");
+});
 
-app.get("/mode", function (req, res, next) {});
+app.get("/mode", function (req, res, next) {
+  let numsAsStrings = req.query.nums.split(",");
+});
 
 app.listen(3000, function () {
   console.log(`Server started on port 3000`);

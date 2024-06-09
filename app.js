f41f681668a95f6f354390b70fdd01f8c1b6c3e8
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 11:49:29 -0400

    implementing route logic for operations

--- app.js
+++ app.js
@@ -11,16 +11,37 @@ const {
 app.get("/mean", function (req, res, next) {
   let numsAsStrings = req.query.nums.split(",");
   let nums = convertAndValidateNumsArray(numsAsStrings);
+
+  let result = {
+    operation: "mean",
+    result: findMean(nums),
+  };
+
+  return res.send(result);
 });
 
 app.get("/median", function (req, res, next) {
   let numsAsStrings = req.query.nums.split(",");
   let nums = convertAndValidateNumsArray(numsAsStrings);
+
+  let result = {
+    operation: "median",
+    result: findMedian(nums),
+  };
+
+  return res.send(result);
 });
 
 app.get("/mode", function (req, res, next) {
   let numsAsStrings = req.query.nums.split(",");
   let nums = convertAndValidateNumsArray(numsAsStrings);
+
+  let result = {
+    operation: "mode",
+    result: findMode(nums),
+  };
+
+  return res.send(result);
 });
 
 app.listen(3000, function () {

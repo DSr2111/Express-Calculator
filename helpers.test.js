7c2bc704bd26d1c4f43afaae9c166755c4b6aa18
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 12:06:17 -0400

    mean and mode tests

--- helpers.test.js
+++ helpers.test.js
@@ -5,3 +5,14 @@ describe("#findMedian", function () {
     expect(findMedian([1, -1, 4, 2])).toEqual(1.5);
   });
 });
+describe("#findMean", function () {
+  it("finds the mean of an empty array", function () {
+    expect(findMean([])).toEqual(0);
+  });
+});
+
+describe("#findMode", function () {
+  it("finds the mode", function () {
+    expect(findMode([1, 1, 1, 1, 2, 3, 3])).toEqual(1);
+  });
+});

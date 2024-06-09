468c967d76c6555e0acd9624b6e7cf622a176ff0
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-09 12:04:36 -0400

    wip

--- helpers.test.js
+++ helpers.test.js
@@ -1,1 +1,7 @@
 const { findMean, findMedian, findMode } = require("./helpers");
+
+describe("#findMedian", function () {
+  it("finds the median of an even set", function () {
+    expect(findMedian([1, -1, 4, 2])).toEqual(1.5);
+  });
+});
